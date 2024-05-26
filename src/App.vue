072eec590d26ae6be713de21d6f270c5b730498b
Tnixc <tnixxc@gmail.com>
2024-05-26 17:55:31 -0400

    Sun May 26 17:55:31 EDT 2024

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,20 @@
 <template>
+  <main class="p-4">
     <div>
-        <button v-if="!user" @mousedown="signIn">Sign In with google</button>
+      <button
+        class="border rounded-md p-4 cursor-pointer flex gap-3 items-center justify-center"
+        v-if="!user"
+        @mousedown="signIn"
+      >
+        <AntDesignGoogleCircleFilled class="size-6" /> Sign In with google
+      </button>
     </div>
     <Dashboard />
+  </main>
 </template>
 
 <script setup>
+import AntDesignGoogleCircleFilled from "./components/icones/AntDesignGoogleCircleFilled.vue";
 import Dashboard from "@/components/Dashboard.vue";
 import { inject, computed } from "vue";
 import { useAuthStore } from "@/store/auth";
@@ -15,8 +24,11 @@ const authStore = useAuthStore();
 const user = computed(() => authStore.user);
 
 async function signIn() {
-    const { data, error } = await supabase.auth.signInWithOAuth({
-        provider: "google", // Or any other provider you have setup for OAuth
-    });
+  const { data, error } = await supabase.auth.signInWithOAuth({
+    provider: "google", // Or any other provider you have setup for OAuth
+  });
+  if (error) {
+    console.error(error);
+  }
 }
 </script>
--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,8 @@ import App from "./App.vue";
 import { createPinia } from "pinia";
 import { createClient } from "@supabase/supabase-js";
 import { useAuthStore } from "./store/auth";
-
+import "./style.css"
+import "./ui.css"
 const supabase = createClient(
   "https://culcnkusqdekedddmute.supabase.co",
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN1bGNua3VzcWRla2VkZGRtdXRlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDgyNzcwMzYsImV4cCI6MjAyMzg1MzAzNn0.8iIx6tF_ZUAAqQO2pN97WKBkaC0k5KODxP6v_37wTSc",
@@ -16,7 +17,7 @@ app.provide("supabase", supabase);
 supabase.auth.onAuthStateChange((event, session) => {
   const authStore = useAuthStore();
   if (event === "SIGNED_IN") {
-    authStore.setUser(session.user);
+    authStore.setUser(session?.user);
   } else if (event !== "INITIAL_SESSION") {
     authStore.setUser(null);
   }
